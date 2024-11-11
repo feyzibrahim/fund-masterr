64d867d7bf647fc88057e1e9345b374bbf37af8a
Feyz Ibrahim <faiz.ashraf@basys.ai>
2024-11-11 14:47:29 +0530

    feat: login and sign up page

--- fund-masterr-web/src/app/(auth)/login/page.tsx
+++ fund-masterr-web/src/app/(auth)/login/page.tsx
@@ -1,3 +1,46 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import Link from "next/link";
+import { BsApple, BsGoogle, BsMeta } from "react-icons/bs";
+
 export default function LoginPage() {
-	return <div className="login-page"></div>;
+	return (
+		<div className="login-page grid grid-cols-1 md:grid-cols-2 pt-20 min-h-screen px-5 lg:px-40">
+			<div className="py-5 md:px-20 flex flex-col justify-center gap-5 ">
+				<h1 className="text-2xl font-bold">Login</h1>
+				<p className="text-foreground-secondary">
+					Login to access your Fund Masterr account
+				</p>
+				<Input placeholder="Enter your email" />
+				<Input placeholder="Enter your password" />
+				<Button className="w-full">Submit</Button>
+				<p className="text-center">
+					Don&apos;t have an account?{" "}
+					<Link href="/sign-up" className="text-blue-500">
+						Sign Up
+					</Link>
+				</p>
+				<div className="flex gap-2 items-center">
+					<div className="h-[1px] w-full bg-foreground-secondary rounded-full"></div>
+					<p className="flex-shrink-0 text-xs">Or login with</p>
+					<div className="h-[1px] w-full bg-foreground-secondary rounded-full"></div>
+				</div>
+				<div className="flex gap-5 items-center">
+					<Button variant="outline" size="icon" className="w-full">
+						<BsMeta />
+					</Button>
+					<Button variant="outline" size="icon" className="w-full">
+						<BsGoogle />
+					</Button>
+					<Button variant="outline" size="icon" className="w-full">
+						<BsApple />
+					</Button>
+				</div>
+			</div>
+			<div className="flex items-center justify-center ">
+				<Image src="/login-bg.png" alt="mobile1" width={416} height={616} />
+			</div>
+		</div>
+	);
 }
--- fund-masterr-web/src/app/(auth)/sign-up/page.tsx
+++ fund-masterr-web/src/app/(auth)/sign-up/page.tsx
@@ -1,3 +1,46 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import Link from "next/link";
+import { BsApple, BsGoogle, BsMeta } from "react-icons/bs";
+
 export default function SignUpPage() {
-	return <div></div>;
+	return (
+		<div className="login-page grid grid-cols-1 md:grid-cols-2 pt-20 min-h-screen px-5 lg:px-40">
+			<div className="py-5 md:px-10 flex flex-col justify-center gap-5 ">
+				<h1 className="text-2xl font-bold">Sign Up</h1>
+				<p className="text-foreground-secondary">
+					Let&apos;s get you all set up so you can access your personal account.
+				</p>
+				<Input placeholder="Enter your email" />
+				<Input placeholder="Enter your password" />
+				<Button className="w-full">Submit</Button>
+				<p className="text-center">
+					Already have an account?{" "}
+					<Link href="/sign-up" className="text-blue-500">
+						Login
+					</Link>
+				</p>
+				<div className="flex gap-2 items-center">
+					<div className="h-[1px] w-full bg-foreground-secondary rounded-full"></div>
+					<p className="flex-shrink-0 text-xs">Or login with</p>
+					<div className="h-[1px] w-full bg-foreground-secondary rounded-full"></div>
+				</div>
+				<div className="flex gap-5 items-center">
+					<Button variant="outline" size="icon" className="w-full">
+						<BsMeta />
+					</Button>
+					<Button variant="outline" size="icon" className="w-full">
+						<BsGoogle />
+					</Button>
+					<Button variant="outline" size="icon" className="w-full">
+						<BsApple />
+					</Button>
+				</div>
+			</div>
+			<div className="flex items-center justify-center ">
+				<Image src="/sign-up-bg.png" alt="mobile1" width={287} height={416} />
+			</div>
+		</div>
+	);
 }
