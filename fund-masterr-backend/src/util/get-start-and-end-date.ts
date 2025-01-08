3534541a9ed281ae8e840db5fa4e0e797f5ab891
faiz-basys <faiz.ashraf@basys.ai>
2025-01-08 21:42:09 +0530

    fix: date issue in singapure

--- fund-masterr-backend/src/util/get-start-and-end-date.ts
+++ fund-masterr-backend/src/util/get-start-and-end-date.ts
@@ -6,12 +6,18 @@ export const getStartAndEndDate = (
 ): { start: Date; end: Date } => {
 	const queryDate = req.query.date ? new Date(req.query.date as string) : new Date();
 
-	// Set start and end of the day for the specified date
-	const start = new Date(queryDate);
-	start.setHours(0, 0, 0, 0);
+	// Helper function to adjust the date to IST
+	const adjustToIST = (date: Date) => {
+		const offset = 5.5 * 60 * 60 * 1000; // IST offset in milliseconds
+		return new Date(date.getTime() + offset);
+	};
 
-	const end = new Date(queryDate);
-	end.setHours(23, 59, 59, 999);
+	// Set start and end of the day for the specified date
+	const start = adjustToIST(new Date(queryDate));
+	start.setUTCHours(0, 0, 0, 0); // Set to start of the day in IST
+
+	const end = adjustToIST(new Date(queryDate));
+	end.setUTCHours(23, 59, 59, 999); // Set to end of the day in IST
 
 	return { start, end };
 };
